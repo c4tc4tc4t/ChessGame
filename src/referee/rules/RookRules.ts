fbcc1a3c7ca52a3434d4d31e6c9e4f8380e1a43e
c4tc4tc4t <emilano.ito@gmail.com>
2023-12-24 13:45:48 -0300

    rookrulestest criado

--- src/referee/rules/RookRules.ts
+++ src/referee/rules/RookRules.ts
@@ -1,45 +1,52 @@
 import { TeamType } from "../../Types";
 import { Piece, Position } from "../../models";
-import { tileIsEmptyOrOccupiedByOpponent, tileIsOccupied, tileIsOccupiedByOpponent } from "./GeneralRules";
+import {
+  tileIsEmptyOrOccupiedByOpponent,
+  tileIsOccupied,
+  tileIsOccupiedByOpponent,
+} from "./GeneralRules";
 
-export const rookMove = (initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean => {
-  if (initialPosition.x === desiredPosition.x) {
-    for (let i = 1; i < 8; i++) {
-      let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+// export const rookMove = (initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean => {
+//   if (initialPosition.x === desiredPosition.x) {
+//     for (let i = 1; i < 8; i++) {
+//       let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
 
-      let passedPosition = new Position(initialPosition.x, initialPosition.y + (i * multiplier));
-      if (passedPosition.samePosition(desiredPosition)) {
-        if (tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
-          return true;
-        }
-      } else {
-        if (tileIsOccupied(passedPosition, boardState)) {
-          break;
-        }
-      }
-    }
-  }
+//       let passedPosition = new Position(initialPosition.x, initialPosition.y + (i * multiplier));
+//       if (passedPosition.samePosition(desiredPosition)) {
+//         if (tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+//           return true;
+//         }
+//       } else {
+//         if (tileIsOccupied(passedPosition, boardState)) {
+//           break;
+//         }
+//       }
+//     }
+//   }
 
-  if (initialPosition.y === desiredPosition.y) {
-    for (let i = 1; i < 8; i++) {
-      let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
+//   if (initialPosition.y === desiredPosition.y) {
+//     for (let i = 1; i < 8; i++) {
+//       let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
 
-      let passedPosition = new Position(initialPosition.x + (i * multiplier), initialPosition.y);
-      if (passedPosition.samePosition(desiredPosition)) {
-        if (tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
-          return true;
-        }
-      } else {
-        if (tileIsOccupied(passedPosition, boardState)) {
-          break;
-        }
-      }
-    }
-  }
-  return false;
-}
+//       let passedPosition = new Position(initialPosition.x + (i * multiplier), initialPosition.y);
+//       if (passedPosition.samePosition(desiredPosition)) {
+//         if (tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+//           return true;
+//         }
+//       } else {
+//         if (tileIsOccupied(passedPosition, boardState)) {
+//           break;
+//         }
+//       }
+//     }
+//   }
+//   return false;
+// }
 
-export const GetPossibleRookMoves = (rook: Piece, boardstate: Piece[]): Position[] => {
+export const GetPossibleRookMoves = (
+  rook: Piece,
+  boardstate: Piece[]
+): Position[] => {
   const possibleMoves: Position[] = [];
 
   // Top movement
@@ -110,4 +117,4 @@ export const GetPossibleRookMoves = (rook: Piece, boardstate: Piece[]): Position
   }
 
   return possibleMoves;
-}
+};
